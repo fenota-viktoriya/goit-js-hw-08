4eb7839fc1c69c97d4d345842d8da873ef045420
fenota-viktoriya <Fen9206@gmail.com>
2022-02-19 03:30:38 +0200

    add 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+ //инициализируем Player
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+let currentTimePlay = 0;
+ 
+//присваив. ключ хранилищу
+const STORAGE_KEY = 'videoplayer-current-time';
+
+//вешаем слушателя по событию и отложенною колбэк функцию
+player.on('play', throttle((onPlay),1000));
+//возвращаем текущее время с хранилища после перезагрузки стриницы
+setCurrentTime();
+
+//колбэк функция
+function onPlay(data){
+
+currentTimePlay = data.seconds;
+localStorage.setItem(STORAGE_KEY, currentTimePlay);
+
+};
+
+ function setCurrentTime(){
+    const setTime = localStorage.getItem(STORAGE_KEY);
+    if(setTime){
+        player.setCurrentTime(setTime);
+    }
+ }
