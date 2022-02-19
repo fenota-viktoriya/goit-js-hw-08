266b2caa7638a2197489afe35c4cff4b75dae56b
fenota-viktoriya <Fen9206@gmail.com>
2022-02-19 02:57:40 +0200

    add 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,23 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+//1 Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+const galleryRef = document.querySelector('.gallery');
+function createMarkup({ preview, original, description}){
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`
+};
+const markup = galleryItems.map(createMarkup).join('');
+galleryRef.insertAdjacentHTML('afterbegin', markup);
+
+let lightbox = new SimpleLightbox('.gallery a');
+//console.dir(lightbox);
+lightbox.defaultOptions.captionDelay = 250;
+lightbox.defaultOptions.captionsData = 'Alt';
